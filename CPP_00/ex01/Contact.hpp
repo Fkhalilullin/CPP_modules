c51340f31ae2c47827707c0fac72326b9a1d280d
Naburi <58636313+Naburi@users.noreply.github.com>
2021-07-03 17:50:40 +0300

    complete cpp00 ex01

--- CPP_00/ex01/Contact.hpp
+++ CPP_00/ex01/Contact.hpp
@@ -17,22 +17,23 @@
 
 class Contact
 {
-private:
-    std::string first_name;
-    std::string last_name;
-    std::string nickname;
-    std::string phone_number;
-    std::string darkest_secret;
-public:
-    Contact();
-    ~Contact();
-    void AddFirstName();
-    void AddLastName();
-    void AddNickname();
-    void AddPhoneNumber();
-    void AddDarkestSecret();
-    void PrintList();
-    void PrintInfo(int index);
+    private:
+        std::string first_name;
+        std::string last_name;
+        std::string nickname;
+        std::string phone_number;
+        std::string darkest_secret;
+    public:
+        Contact();
+        ~Contact();
+        
+        void AddFirstName();
+        void AddLastName();
+        void AddNickname();
+        void AddPhoneNumber();
+        void AddDarkestSecret();
+        void PrintList();
+        void PrintInfo(int index);
 };
 
 #endif
