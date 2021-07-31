29d5ba2193198eb7a8b7a73589af16eab0a1261d
Makeda Teressa <mteressa@pl-g1.msk.21-school.ru>
2021-07-31 14:44:33 +0300

    finish module_04 ex00

---
+++ module_04/ex00/Dog.cpp
@@ -0,0 +1,18 @@
+#include "Dog.hpp"
+
+Dog::Dog() : Animal() {
+	this->_type = "Scooby";
+}
+Dog::~Dog() {}
+Dog::Dog(const Dog &src) {
+	*this = src;
+}
+
+Dog & Dog::operator=(Dog const &rhs) {
+	this->_type = rhs.getType();
+	return *this;
+}
+
+void Dog::makeSound() const {
+	std::cout << "Scooby Dooby DooOOooOO" << std::endl;
+}
